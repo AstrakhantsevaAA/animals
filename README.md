bf32b5836029c1120c0fd1fe6fc9db2776fd0f7e
AstrakhantsevaAA <astra92293@gmail.com>
2022-09-19 16:38:36 +0200

    add setup and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+Classification of the wildlife species that appear in camera trap images
+
+[Competition](https://www.drivendata.org/competitions/87/competition-image-classification-wildlife-conservation/page/409/)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="animals",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Classification of the wildlife species",
+    author="AstrakhantsevaAA",
+    license="",
+)
